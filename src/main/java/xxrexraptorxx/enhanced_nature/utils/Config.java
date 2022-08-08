7395ed7b569b6c715dc85209c2f27fc007ad1271
XxRexRaptorxX <Raptor-Simon@web.de>
2022-08-09 00:05:20 +0200

    config changes

--- src/main/java/xxrexraptorxx/enhanced_nature/utils/Config.java
+++ src/main/java/xxrexraptorxx/enhanced_nature/utils/Config.java
@@ -15,7 +15,8 @@ public class Config {
     public static ForgeConfigSpec COMMON_CONFIG;
 
     public static ForgeConfigSpec.BooleanValue UPDATE_CHECKER;
-    public static ForgeConfigSpec.BooleanValue WORLD_GENERATION;
+    public static ForgeConfigSpec.BooleanValue PATREON_REWARDS;
+
     public static ForgeConfigSpec.IntValue SANDSTONE_VEIN_RARITY;
     public static ForgeConfigSpec.IntValue CLAY_VEIN_RARITY;
     public static ForgeConfigSpec.IntValue DEEPSLATE_VEIN_RARITY;
@@ -24,7 +25,6 @@ public class Config {
     public static ForgeConfigSpec.IntValue STONE_VEIN_RARITY;
     public static ForgeConfigSpec.IntValue SILVERFISH_BLOCK_VEIN_RARITY;
     public static ForgeConfigSpec.BooleanValue GENERATE_QUICKSAND;
-    public static ForgeConfigSpec.BooleanValue GENERATE_MUD;
 
 
     public static void init() {
@@ -50,8 +50,12 @@ public class Config {
     public static void initCommon() {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
+        builder.comment("General").push(CATEGORY_GENERAL);
+        PATREON_REWARDS = builder.comment("Enables ingame rewards on first spawn for Patreons").define("patreon_rewards", true);
+        builder.pop();
+
+        /**     TODO
         builder.comment("World").push(CATEGORY_WORLD);
-        WORLD_GENERATION = builder.comment("Activate the world generation features of Enhanced Nature ").define("world_generation", true);
         CLAY_VEIN_RARITY = builder.comment("The rarity of Clay veins (veins per chunk), 0 = no veins").defineInRange("clay_vein_rarity", 10, 0, 100);
         SAND_VEIN_RARITY = builder.comment("The rarity of Sand veins (veins per chunk), 0 = no veins").defineInRange("sand_vein_rarity", 10, 0, 100);
         SANDSTONE_VEIN_RARITY = builder.comment("The rarity of Sandstone veins (veins per chunk), 0 = no veins").defineInRange("sandstone_vein_rarity", 10, 0, 100);
@@ -60,7 +64,8 @@ public class Config {
         DEEPSLATE_VEIN_RARITY = builder.comment("The rarity of Clay veins (veins per chunk), 0 = no veins").defineInRange("deepslate_vein_rarity", 1, 0, 100);
         SILVERFISH_BLOCK_VEIN_RARITY = builder.comment("The rarity of Silverfish block veins (veins per chunk), 0 = no veins").defineInRange("silverfish_vein_rarity", 5, 0, 100);
         GENERATE_QUICKSAND = builder.comment("Activate/Deactivate Quicksand generation in your world").define("quicksand_generation", true);
-        GENERATE_MUD = builder.comment("Activate/Deactivate Mud generation in your world").define("mud_generation", true);
+        builder.pop();
+        **/
         COMMON_CONFIG = builder.build();
     }
 
