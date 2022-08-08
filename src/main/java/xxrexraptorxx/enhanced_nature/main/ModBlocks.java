0714b7a12d0b837e9410706e8bd76a51a2ee8099
XxRexRaptorxX <Raptor-Simon@web.de>
2022-08-09 00:03:20 +0200

    mud removed

--- src/main/java/xxrexraptorxx/enhanced_nature/main/ModBlocks.java
+++ src/main/java/xxrexraptorxx/enhanced_nature/main/ModBlocks.java
@@ -1,19 +1,14 @@
 package xxrexraptorxx.enhanced_nature.main;
 
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.FlowerBlock;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import xxrexraptorxx.enhanced_nature.blocks.BlockMud;
 import xxrexraptorxx.enhanced_nature.blocks.BlockQuicksand;
 
 public class ModBlocks {
@@ -27,10 +22,6 @@ public class ModBlocks {
         BLOCKS.register(bus);
         ITEMS.register(bus);
     }
-
-
-    public static final RegistryObject<BlockMud> MUD = BLOCKS.register("mud", BlockMud::new);
-    public static final RegistryObject<Item> MUD_BLOCKITEM = ITEMS.register("mud", () -> new BlockItem(MUD.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
 
     public static final RegistryObject<BlockQuicksand> QUICK_SAND = BLOCKS.register("quicksand", BlockQuicksand::new);
     public static final RegistryObject<Item> QUICK_SAND_BLOCKITEM = ITEMS.register("quicksand", () -> new BlockItem(QUICK_SAND.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
